90fbe26d48650ec5feff2550daf6531ba282b5ad
jessie-chuang <chy_jessie@hotmail.com>
2017-03-06 23:50:27 +0800

    hw1_105753008

---
+++ hw1/hw1_105753008.R
@@ -0,0 +1,38 @@
+########################
+# homework1 example
+########################
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("USAGE: Rscript hw1_105753008.R  -files test.1.csv -out result.csv", call.=FALSE)
+} else if (length(args)==1) {
+  i_f <- args[1] 
+}
+
+	# parse parameters
+i<-1 
+while(i < length(args))
+{
+  if(args[i] == "-files"){
+    i_f<-args[i+1]
+    i<-i+1
+  }else if(args[i] == "-out"){
+    o_f<-args[i+1]
+    i<-i+1
+  }else{
+    stop(paste("Unknown flag", args[i]), call.=FALSE)
+  }
+  i<-i+1
+}
+name <- i_f
+
+	#Read in a input file--test.1.csv
+data <- read.csv(i_f)
+
+	#max
+weightmax <- apply(data[2],2,max)
+heightmax <- apply(data[3],2,max)
+
+	#Output summary file--result.csv
+out <- data.frame(set=name,weight=weightmax,height=heightmax)
+write.csv(out, file = "result.csv", row.names = FALSE)
