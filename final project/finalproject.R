7e28a202749018528ce28e837189b4ae77b8ec24
jessie-chuang <chy_jessie@hotmail.com>
2017-06-20 15:26:53 +0800

    add picture

--- final project/finalproject.R
+++ final project/finalproject.R
@@ -6,9 +6,14 @@ library("ggplot2")
 
 #read files
 data <- read.csv("https://raw.githubusercontent.com/jessie-chuang/1052DataScience/master/final%20project/Titanic.csv")
-
 #read field name
+ ggplot(data = data, aes(x = Sex, fill = factor(PClass))) +
+  geom_bar(position = "dodge") +
+  ggtitle('position = "dodge"')
+
+
 data1 <- subset(data, select = c(PClass, Age, Sex, Survived))
+#SurvivedAvg<-summarise(SurvivedAvg = mean(Survived))
 
 #change value type
 data1$Survived <- ifelse(data1$Survived == 1, "Yes", "No");
@@ -16,12 +21,28 @@ data1$Age <- ifelse(data1$Age >= 18, "Adult", "Child");
 
 #delete NA value
 finaldata <- data1[complete.cases(data1), ]
+ggplot(data = finaldata, aes(x = Sex, fill = factor(PClass))) +
+  geom_bar(position = "dodge") +
+  ggtitle('position = "dodge"')
+
+
 
 #test/train data (8:2)
 set.seed(22)
 train.index <- sample(x=1:nrow(finaldata), size=ceiling(0.8*nrow(finaldata) ))
 train <- finaldata[train.index, ]
 test <- finaldata[-train.index, ]
+
+
+ggplot(data =data, aes(x = Age, y = Survived, color = Sex)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth(se = FALSE) +
+  facet_grid(PClass ~ .) +
+  theme_classic() +
+  theme(text = element_text(size=30)) +
+  scale_color_manual(values = c("#EF5350", "#64B5F6"))
+
+
 
 # CART model-Y for survived
 cart.model<- rpart(Survived ~. ,  data=train)
@@ -48,5 +69,4 @@ Precision <- TP/(TP+FP)
 Precision
 Recall <- TP/(TP+FN)
 Recall
-
 
